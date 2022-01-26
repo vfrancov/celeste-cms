d2f85105197e311c3b977b9e93303ba3247a9d18
Victor Daniel Franco Valencia <vfrancov25@gmail.com>
2022-01-25 21:51:59 -0500

    Validaciones en Formulario Empresa

--- src/app/core/constants/companies.field.ts
+++ src/app/core/constants/companies.field.ts
@@ -3,8 +3,8 @@ import { CreateCompanieForm } from "@core/validators/companies.validators";
 
 export const CompaniesField: CreateCompanieForm = {
   id: [0],
-  name: ['', Validators.required],
-  nit: ['', Validators.required],
+  name: ['', [Validators.required, Validators.min(2)]],
+  nit: ['', [Validators.required, Validators.min(3)]],
   email: ['', [Validators.required, Validators.email]],
   address: ['', Validators.required],
   phoneNumber: ['', Validators.required],
--- src/app/core/validators/companies.validators.ts
+++ src/app/core/validators/companies.validators.ts
@@ -2,8 +2,8 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export interface ICompaniesField {
   id?: (number | ((control: AbstractControl) => ValidationErrors))[],
-  name: (string | ((control: AbstractControl) => ValidationErrors))[]
-  nit: (string | ((control: AbstractControl) => ValidationErrors))[]
+  name: (string | ((control: AbstractControl) => ValidationErrors)[])[]
+  nit: (string | ((control: AbstractControl) => ValidationErrors)[])[]
   email: (string | ((control: AbstractControl) => ValidationErrors)[])[]
   address: (string | ((control: AbstractControl) => ValidationErrors))[]
   phoneNumber: (string | ((control: AbstractControl) => ValidationErrors))[]
